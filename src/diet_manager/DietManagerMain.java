ad7796ff72c28c866d9520e2cb812c383b31300d
parkjongwoo <shoong1999@gmail.com>
2018-07-16 14:13:07 +0900

    first commit
    add ignores

---
+++ src/diet_manager/DietManagerMain.java
@@ -0,0 +1,12 @@
+package diet_manager;
+
+import javax.swing.JFrame;
+
+public class DietManagerMain extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
